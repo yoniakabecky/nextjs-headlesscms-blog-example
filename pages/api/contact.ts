d25501b74b4850b415bc1d3c945fa4c0cdea10f4
Yoni <yoniakabecky@gmail.com>
2021-05-10 11:08:19 -0700

    Change contact api response

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -2,29 +2,34 @@ import { NextApiRequest, NextApiResponse } from "next";
 import nodemailer from "nodemailer";
 
 export default function (req: NextApiRequest, res: NextApiResponse) {
+  const { name, email, message } = req.body;
+
   const transporter = nodemailer.createTransport({
     port: 465,
     secure: true, // true for 465, false for other ports
     host: "smtp.gmail.com",
     auth: {
-      user: "test.yoniakabecky@gmail.com", // less secure GMail
+      user: process.env.EMAIL, // less secure GMail
       pass: process.env.PASSWORD,
     },
   });
 
-  const mailData = {
-    from: "My Test account",
-    to: "test.yoniakabecky@gmail.com",
-    subject: `Message From ${req.body.name}`,
-    text: req.body.message + " | Sent from: " + req.body.email,
-    html: `<div>${req.body.message}</div>
-    <p>From: ${req.body.name} (${req.body.email})</p>`,
+  const mailOption = {
+    from: `${email}`,
+    to: `${process.env.EMAIL}`,
+    subject: `Message From ${name}`,
+    text: `${message} | Sent from: ${name} <${email}>`,
+    html: `<div>${message}</div>
+    <p>From ${name} -${email}-</p>`,
   };
 
-  transporter.sendMail(mailData, function (err, info) {
-    if (err) console.error(err);
-    else console.info(info);
+  transporter.sendMail(mailOption, function (err, info) {
+    if (err) {
+      console.error(err);
+      res.send("error" + JSON.stringify(err));
+    } else {
+      console.log(info.messageId);
+      res.status(200).json({ message: "email sent" });
+    }
   });
-
-  res.status(200).json({ message: "email sent" });
 }
